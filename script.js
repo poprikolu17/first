426aba3ff24dc9a454d2f40a1298517cf850d11d
tarazchanin <shinobitaraz@gmail.com>
2022-09-18 16:56:28 +0600

    firsr practice

---
+++ script.js
@@ -0,0 +1,21 @@
+let numberOfFilms = +prompt("Сколько фильмов уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = prompt("На сколько оцените его?", ""),
+      c = prompt("Один из последних просмтренных фильмов?", ""),
+      d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
